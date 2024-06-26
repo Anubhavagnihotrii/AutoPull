6ca9430140f0112a861ee3de8bed451679333be7
Anubhav <anubahvagnihotri@gmail.com>
2024-06-26 23:04:06 +0530

    adding code to auto pull changes everyday from repo

---
+++ pull_changes.bat
@@ -0,0 +1,14 @@
+@echo off
+cd C:\Users\anubhav\Desktop\myAutoRepo\Java || (
+    echo Failed to change directory. >> C:\Users\anubhav\Desktop\tryAutomate\logfile.txt
+    exit /b 1
+)
+git pull origin main || (
+    echo Git pull failed. >> C:\Users\anubhav\Desktop\tryAutomate\logfile.txt
+    exit /b 1
+)
+
+powershell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('The repository has been updated successfully.')"
+if %errorlevel% neq 0 (
+    echo PowerShell notification failed. >> C:\Users\anubhav\Desktop\tryAutomate\logfile.txt
+)
