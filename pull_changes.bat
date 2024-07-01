510207f44f22d82e661c92a1daa5c767332c028f
Anubhav <anubahvagnihotri@gmail.com>
2024-07-01 22:28:22 +0530

    fixing switching branch before pulling to avoid conflicts

--- pull_changes.bat
+++ pull_changes.bat
@@ -3,6 +3,7 @@ cd C:\Users\anubhav\Desktop\myAutoRepo\Java || (
     echo Failed to change directory. >> C:\Users\anubhav\Desktop\tryAutomate\logfile.txt
     exit /b 1
 )
+git switch main
 git pull origin main || (
     echo Git pull failed. >> C:\Users\anubhav\Desktop\tryAutomate\logfile.txt
     exit /b 1
